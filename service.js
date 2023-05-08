e1fc044eb5aa2ea4931e415e03d57c3c83b878b8
nagaraj6618 <nagaraj516700@gmail.com>
2023-05-08 11:15:11 +0530

    dynamic and services added in Service page

--- service.js
+++ service.js
@@ -3,4 +3,34 @@ const router=express.Router()
 router.get('/',(req,res)=>{
   res.send("Service Page")
 })
+
+router.get('/:id',(req,res)=>{
+const pageId=Number(req.params.id);
+  const serviceId=service.find((ser)=>ser.id===pageId);
+  if(!serviceId){
+    res.send("Service Not Found")
+  }
+  else{
+    res.json(service[pageId])
+  }
+})
+const service=[
+  {
+    id:0,
+    name:'App development'
+  },
+  {
+    id:1,
+    name:'Web development'
+  },
+  {
+    id:2,
+    name:'Iot development'
+  },
+  {
+    id:3,
+    name:'Embedded system'
+
+  }
+]
 module.exports=router;
--- user.js
+++ user.js
@@ -3,4 +3,44 @@ const router=express.Router()
 router.get('/',(req,res)=>{
   res.send("User Page")
 })
+
+router.get('/:id([0-9])',(req,res)=>{
+const pageId=Number(req.params.id)
+const userId=users.find((user)=>(user.idno-1)===pageId)
+
+if(!userId){
+  res.send('Page Not Found')
+}
+else{
+  res.json(users[req.params.id])
+}
+  // if(req.params.id<=UsersArrayLength){
+  //   res.json(users[req.params.id-1])
+  //  res.send(`User Name for this id ${users[req.params.id-1].idno} is ${users[req.params.id-1].name}`)
+  // }
+  // else{
+  //   res.send("Wrong id")
+  // }
+  // // res.send(`List of Users ${req.params.id}`)
+})
+
+
+const users=[{
+  idno:1,
+  name:'Nagaraj'
+},
+{
+  idno:2,
+  name:'Sakthivel'
+},
+{
+  idno:3,
+  name:'Sureka'
+},
+{
+  idno:4,
+  name:'Trisha'
+}]
+
+// const UsersArrayLength=users.length;
 module.exports=router;
