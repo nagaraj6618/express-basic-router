9a94773dbff5840a244a1098ea738ce1de0ebe55
nagaraj6618 <nagaraj516700@gmail.com>
2023-05-08 09:44:04 +0530

    multiple Router created

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const app=express();
 const userPage=require('./user')
+const contactPage=require('./contact')
+const aboutPage=require('./about')
+const servicePage=require('./service')
 app.get('/',(req,res)=>{
   res.send("Home Page");
 })
 
 app.use("/user",userPage)
+app.use('/contact',contactPage)
+app.use('/aboutus',aboutPage)
+app.use('/service',servicePage)
 app.listen(5000);
